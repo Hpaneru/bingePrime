05a422543386d40152fda3182b5155ac9c14f303
Hemant Paneru <hpaneru.hp@yahoo.com>
2020-10-15 10:29:18 +0545

    #1

---
+++ android/app/src/main/kotlin/com/example/binge_prime/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.binge_prime
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:binge_prime/screens/home.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
